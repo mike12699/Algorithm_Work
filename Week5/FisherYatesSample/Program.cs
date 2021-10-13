348dec7cf69ff436b1405fcee7ce20ef94445f3d
mike12699 <mariomania85@gmail.com>
2021-10-12 23:03:54 -0500

    Adds the Week 5 content

---
+++ Week5/FisherYatesSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYatesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Random random = new Random();
+            array = array.OrderBy(x => random.Next()).ToArray();
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week5/README.md
@@ -0,0 +1,3 @@
+### Fisher-Yates Sample Work
+
+In this assignment, I implemented a very simple shuffle method to an array of numbers. I used the _Random_ variable to sort out the numbers. By using the _foreach_ method, I called in the next random integer to shuffle the array.
