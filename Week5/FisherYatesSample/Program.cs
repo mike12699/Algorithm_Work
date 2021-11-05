6998ce265901b6a022c0b09f84579f9723a8bd8c
mike12699 <mariomania85@gmail.com>
2021-11-05 18:01:14 -0500

    Adds the comments to project

--- Week5/FisherYatesSample/Program.cs
+++ Week5/FisherYatesSample/Program.cs
@@ -8,6 +8,11 @@ namespace FisherYatesSample
 {
     class Program
     {
+        /// <summary>
+        /// In this example, I created a simple array along with a "random" variable that would suffle in this case.
+        /// The numbers listed are from 1 through 5 and they will be randomly shuffled when the application is being run.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4, 5 };
